6f73674186e69fbccdbf1f92f506e2c57d1616fe
radek-git <radoslaw.szpionkowski@gmail.com>
2020-12-12 11:20:16 +0100

    Add new methods

--- src/com/MovieApplication.java
+++ src/com/MovieApplication.java
@@ -1,9 +1,11 @@
 package com;
 
+import static com.MovieDB.*;
+
 public class MovieApplication {
 
     public static void main(String[] args) {
-        MovieDB.dataPreparation();
-        MovieDB.menuSelection();
+        dataPreparation();
+        menuSelection();
     }
 }
--- src/com/MovieDB.java
+++ src/com/MovieDB.java
@@ -13,8 +13,7 @@ public class MovieDB {
         Scanner scanner = new Scanner(System.in);
         int counter = 0;
         while (counter == 0) {
-            System.out.println(
-                    "1. Dodaj Film\n" +
+            System.out.println("1. Dodaj Film\n" +
                             "2. Szukaj filmu po nazwie\n" +
                             "3. Wypisz filmy z zakresu dat\n" +
                             "4. Sprawdz w jakich filmach grał dany aktor\n" +
@@ -23,15 +22,19 @@ public class MovieDB {
             int selectMenu = scanner.nextInt();
             switch (selectMenu) {
                 case 1:
+                    addMovie(movieList);
                     break;
                 case 2:
                     findMovieByTitle(movieList);
                     break;
                 case 3:
+                    searchMovieFromRange();
                     break;
                 case 4:
+                    checkMoviesForActor(movieList);
                     break;
                 case 5:
+                    checkMovieBudget(movieList);
                     break;
                 default:
                     System.out.println("Zły wybór");
@@ -45,9 +48,9 @@ public class MovieDB {
         Director stevenSpilberg = new Director("Steven", "Spilberg", 2000);
         Director jamesCameron = new Director("James", "Cameron", 3000);
 
-        Actor tomCruiz = new Actor("Tom", "Cruiz", 1000);
+        Actor tomCruiz = new Actor("Tom", "Cruize", 1000);
         Actor juliaRoberts = new Actor("Juila", "Roberts", 1100);
-        Actor stevenSigal = new Actor("Steven", "Sigal", 1200);
+        Actor stevenSigal = new Actor("Steven", "Seagal", 1200);
 
         Movie titanic = new Movie("Titanic", stevenSpilberg,
                 LocalDate.of(2020, 12, 1), Arrays.asList(tomCruiz, juliaRoberts), MovieType.action);
@@ -66,6 +69,86 @@ public class MovieDB {
             if (movie.getTitle().equals(movieTitle)) {
                 String toString = movie.toString();
                 System.out.println(toString);
+            }
+        }
+    }
+
+
+    private static void addMovie(List<Movie> movies) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj tytuł filmu");
+        String title = sc.nextLine();
+        movies.add(new Movie(title, null, null, null, MovieType.comedy));
+
+        for (Movie movie : movies) {
+            System.out.println(movie);
+        }
+    }
+
+
+
+//    public static void findListOfMoviesByActor(List<Movie> movieList) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Podaj nazwisko aktora");
+//        String actorLastName = sc.nextLine();
+//
+//        List<Movie> movieWithActor =  movieList.stream()
+//                .filter(movie -> movie.getActors().stream()
+//                        .anyMatch(actor -> actor.getLastName().equals(actorLastName)))
+//                .collect(Collectors.toList());
+//
+//        for (Movie movie : movieWithActor) {
+//            System.out.println(movie);
+//        }
+//    }
+
+
+    private static void checkMoviesForActor(List<Movie> movies) {
+        System.out.println("Podaj imię i nazwisko");
+        Scanner sc = new Scanner(System.in);
+        String firstOrLastName = sc.nextLine();
+
+        for (Movie movie : movies) {
+            for (Actor actor : movie.getActors()) {
+                if (actor.getFirstName().equals(firstOrLastName) || actor.getLastName().equals(firstOrLastName)) {
+                    System.out.println(movie.getTitle());
+                }
+            }
+        }
+    }
+
+
+
+    private static void checkMovieBudget(List<Movie> movieList) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj tytuł filmu");
+        String movieName = sc.nextLine();
+        double budgetMovie = 0;
+
+        for (Movie movie : movieList) {
+            if (movie.getTitle().equals(movieName)) {
+                budgetMovie += movie.getDirector().getPayment();
+
+                for (Actor actor : movie.getActors()) {
+                    budgetMovie += actor.getPayment();
+                }
+                System.out.println(budgetMovie);
+            }
+        }
+    }
+
+
+    private static void searchMovieFromRange() {
+        System.out.println("Podaj datę od w formacie RRRR-MM-DD");
+        Scanner sc = new Scanner(System.in);
+        LocalDate start = LocalDate.parse(sc.nextLine());
+        System.out.println("Podaj datę do w formacie RRRR-MM-DD");
+        LocalDate end = LocalDate.parse(sc.nextLine());
+
+        for (Movie movie : movieList) {
+            if (movie.getPremierDate().isAfter(start) && movie.getPremierDate().isBefore(end) ||
+            movie.getPremierDate().equals(start) || movie.getPremierDate().equals(end)) {
+                System.out.println(movie.getTitle());
             }
         }
     }
